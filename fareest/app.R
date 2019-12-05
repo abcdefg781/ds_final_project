495517e959c399b93ea78dd4d9a0e7259e0d2aac
Ngoc <Seven@Ngocs-MacBook-Pro.local>
2019-12-05 02:22:28 -0500

    commit fare analysis + taxi explore + fare estimator

---
+++ fareest/app.R
@@ -0,0 +1,87 @@
+library(shiny)
+library(tidyverse)
+library(lubridate)
+library(plotly)
+
+taxi_data = read_csv("./data/transport_final.csv")
+
+impute_data = read_csv("./data/impute_final.csv")
+
+fare_data = 
+    taxi_data %>% 
+    separate(pickup_time, into = c("pickup_hr", "pickup_min", "pickup_sec"), sep = ":") %>% 
+    separate(dropoff_time, into = c("dropoff_hr", "dropoff_min", "dropoff_sec"), sep = ":") %>% 
+    select(-pickup_sec, -dropoff_min, -pickup_min, -dropoff_sec) %>% 
+    mutate(
+        pickup_hr = as.numeric(pickup_hr),
+        dropoff_hr = as.numeric(dropoff_hr)) %>% 
+    filter(type != "for hire",
+           pu_boro != "Staten Island") %>% 
+    drop_na(pu_boro)
+
+fare_data_base =
+    fare_data %>% 
+    mutate(time_of_day=
+               cut(pickup_hr, 
+                   breaks = c(0,2,5,9,11,13,16,18,21,23),
+                   labels = c("night","early morning","morning rush","others","lunch","others","evening rush","dinner time","night")),
+           congestion = (fare_amount - trip_distance*2.5 -2.5)/.5) %>% 
+                              filter(congestion>=0) 
+
+fare_data = fare_data_base[-c(123, 16214),]
+                              
+
+pu_neiborhood = impute_final %>% distinct(pu_neiborhood) %>% pull()
+
+do_neiborhood = impute_final %>% distinct(do_neiborhood) %>% pull()
+
+time = fare_data %>% 
+    distinct(time_of_day) %>% 
+    drop_na(time_of_day) %>% 
+    pull()
+
+moderate_lm = lm(fare_amount ~ trip_distance + duration + as.factor(time_of_day), data = fare_data)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Taxi Fare Estimator"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("pu_choice", 
+                        label = h3("Select pick-up neighborhood"),
+                        choices = pu_neiborhood, selected = "Alphabet City"),
+                    
+            selectInput("do_choice", 
+                        label = h3("Select drop-off neighborhood"),
+                        choices = do_neiborhood, selected = "Clinton East"),
+
+            selectInput("time_of_day", 
+                        label = h3("Select current time window"),
+                        choices = time, selected = "dinner")),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           dataTableOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderDataTable({
+        impute_data %>% 
+            filter(do_neiborhood = input[["do_choice"]],
+                   pu_neiborhood = input[["pu_choice"]]) %>%
+            pull(avg_dist, avg_duration) %>% 
+            tibble()
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ timedist/app.R
@@ -0,0 +1,172 @@
+library(shiny)
+library(tidyverse)
+library(readr)
+library(viridis)
+library(flexdashboard)
+library(plotly)
+library(leaflet)
+library(networkD3)
+
+#Load in data 
+
+taxi_data = read_csv("./data/transport_final.csv")
+
+tp_data = 
+    taxi_data %>% 
+    separate(pickup_time, into = c("pickup_hr", "pickup_min", "pickup_sec"), sep = ":") %>% 
+    separate(dropoff_time, into = c("dropoff_hr", "dropoff_min", "dropoff_sec"), sep = ":") %>% 
+    select(-pickup_sec, -dropoff_min, -pickup_min, -dropoff_sec) %>% 
+    mutate(
+        pickup_hr = as.numeric(pickup_hr),
+        dropoff_hr = as.numeric(dropoff_hr)
+    )
+
+boros = tp_data %>% distinct(pu_boro) %>% drop_na(pu_boro) %>% pull()
+
+tp_type = tp_data %>% distinct(type) %>% drop_na(type) %>% pull()
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Taxi Distribution Explorer"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                "boro_choice", 
+                label = h3("Select borough"),
+                choices = boros, selected = "Manhattan"),
+            
+            selectInput(
+                "day",
+                label = h3("Choose day"),
+                choices = c("2019-02-14","2019-02-15"), selected = "2019-02-14"),
+            
+            sliderInput(
+                "time_range", 
+                label = h3("Choose hour range"), 
+                min = 0, max = 23, value = c(18, 21)),
+            radioButtons(
+                "car_type", 
+                label = h3("Select transportation type"),
+                choices = tp_type, selected = "yellow")),
+        
+        # Show a plot of the generated distribution
+        mainPanel(tabsetPanel(type = "tab",
+                              tabPanel("Map drop-offs", leafletOutput("mapPlot")),
+                              tabPanel("Summary drop-offs", plotlyOutput("summary_do")),
+                              tabPanel("Map pick-ups", leafletOutput("mapPlot2")),
+                              tabPanel("Map pick-ups", plotlyOutput("summary_pu"))))
+))
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$mapPlot <- renderLeaflet({
+        tp_data %>%
+            filter(pu_boro == input[["boro_choice"]],
+                   pickup_date == input[["day"]],
+                   pickup_hr %in% input$time_range[1]:input$time_range[2],
+                   type == input$car_type) %>% 
+            group_by(do_neiborhood, x.y, y.y, type) %>% 
+            summarize(num = n(),
+                      avg_distance = round(mean(trip_distance),2),
+                      avg_fare = round(mean(fare_amount),2)) %>% 
+            mutate(avg_fare = ifelse(is.na(avg_fare), 0, avg_fare),
+                   avg_distance = ifelse(is.na(avg_distance), 0, avg_distance)) %>% 
+            mutate(label = str_c("Drop-off neighborhood:", do_neiborhood, 
+                                 "</b><br>", "Count: ", num, " drop-offs",
+                                 "</b><br>", "Mean distance: ", avg_distance, " miles",
+                                 "</b><br>", "Mean fare: $", avg_fare)) %>% 
+            leaflet() %>%
+            addTiles() %>% 
+            addProviderTiles(providers$CartoDB.Positron) %>% 
+            addCircleMarkers(~x.y, ~y.y, 
+                             radius = ~num/20,
+                             popup = ~label,
+                             color = ~type)
+        })
+    
+    
+    output$summary_do <- renderPlotly({
+        ggplotly(tp_data %>%
+            filter(pu_boro == input[["boro_choice"]],
+                   pickup_date == input[["day"]],
+                   pickup_hr %in% input$time_range[1]:input$time_range[2],
+                   type == input$car_type) %>% 
+            group_by(do_neiborhood) %>%
+            summarize(freq = n()) %>% 
+            top_n(n = 15) %>% 
+            mutate(do_neiborhood = fct_reorder(do_neiborhood, freq)) %>% 
+            ggplot(aes(x = do_neiborhood, 
+                       y = freq, 
+                       fill = freq)) +
+            geom_bar(stat = "identity") +
+            viridis::scale_fill_viridis(
+                begin = 1, end = 0) + 
+            coord_flip() +
+            labs(
+                title = "Top 10 drop-off neighborhoods in Manhattan from chosen borough",
+                x = "",
+                y = "Number of drop-offs") + 
+            theme_bw() +   
+            theme(legend.position = "none",    
+                  plot.title = element_text(hjust = 0.8, size=12, face='bold')))
+    })
+        
+        output$mapPlot2 <- renderLeaflet({
+            tp_data %>%
+                filter(pu_boro == input[["boro_choice"]],
+                       pickup_date == input[["day"]],
+                       pickup_hr %in% input$time_range[1]:input$time_range[2],
+                       type == input$car_type) %>% 
+                group_by(pu_boro, pu_neiborhood, x.x, y.x, type) %>% 
+                summarize(numpu = n(),
+                          avg_distance = round(mean(trip_distance),2),
+                          avg_fare = round(mean(fare_amount),2)) %>% 
+                mutate(avg_fare = ifelse(is.na(avg_fare), 0, avg_fare),
+                       avg_distance = ifelse(is.na(avg_distance), 0, avg_distance)) %>% 
+                mutate(text_label = str_c("Pick-up neighborhood:", pu_neiborhood, 
+                                          "</b><br>", "Count: ", numpu, " pick-ups",
+                                          "</b><br>", "Mean distance: ", avg_distance, " miles",
+                                          "</b><br>", "Mean fare: $", avg_fare)) %>% 
+                leaflet() %>%
+                addTiles() %>% 
+                addProviderTiles(providers$CartoDB.Positron) %>% 
+                addCircleMarkers(~x.x, ~y.x, 
+                                 radius = ~numpu/20,
+                                 popup = ~text_label,
+                                 color = ~type)
+    })
+        
+        output$summary_pu <- renderPlotly({
+            ggplotly(tp_data %>%
+                         filter(pu_boro == input[["boro_choice"]],
+                                pickup_date == input[["day"]],
+                                pickup_hr %in% input$time_range[1]:input$time_range[2],
+                                type == input$car_type) %>% 
+                         group_by(pu_neiborhood) %>%
+                         summarize(freq = n()) %>% 
+                         top_n(n = 15) %>% 
+                         mutate(pu_neiborhood = fct_reorder(pu_neiborhood, freq)) %>% 
+                         ggplot(aes(x = pu_neiborhood, 
+                                    y = freq, 
+                                    fill = freq)) +
+                         geom_bar(stat = "identity") +
+                         viridis::scale_fill_viridis(
+                             begin = 1, end = 0) + 
+                         coord_flip() +
+                         labs(
+                             title = "Top 10 pick-up neighborhoods in chosen borough",
+                             x = "",
+                             y = "Number of pick-ups") + 
+                         theme_bw() +   
+                         theme(legend.position = "none",    
+                               plot.title = element_text(hjust = 0.5, size=12, face='bold')))
+        })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
